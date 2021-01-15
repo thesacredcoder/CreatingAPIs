5991a1e29ca62415050b9c9005c5bd287743078e
thescaredhacker <akashsamal4875@gmail.com>
2021-01-15 23:42:27 +0530

    Creatinf APIs and so far done with PUT

---
+++ index.js
@@ -0,0 +1,78 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+  { id: 4, name: "course4" },
+];
+
+//HOME get request
+app.get("/", (req, res) => {
+  res.send("Hello World!!!!");
+});
+
+//All courses get request
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+//Specific get request for courses using route params as id
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("Course not found");
+  res.send(course);
+});
+
+//POST verb request
+app.post("/api/courses", (req, res) => {
+  const { error } = validateCourse(req.body);
+
+  if (error) {
+    // 400 Bad Request
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+
+  courses.push(course);
+  res.send(course);
+});
+
+//UPDATION verb request
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("Course not found");
+
+  //Validate
+  const { error } = validateCourse(req.body); // this is result.error but destructured
+
+  if (error) {
+    // 400 Bad Request
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+//Validation function to make the code readable and presentable
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(course);
+}
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on PORT ${port}`));
